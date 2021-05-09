24ec012ed02c48918b69b747bc719d28cea3caea
Dorien Pine <dorien.pine@gmail.com>
2021-05-09 09:06:46 -0700

    added some styling and background image

--- src/App.js
+++ src/App.js
@@ -1,11 +1,16 @@
 import './App.css';
+import AboutUs from '../src/components/aboutUs.jsx';
 
 function App() {
   return (
     <div className="App">
       <div className="top-container">
-        <h3 className="App-Title">La Santisima</h3>
-        <h4>Gourmet Taco Shop</h4>
+        <h3 className="App-Title">La Santisima
+        <h5 style={{margin: "0"}}>Gourmet Taco Shop</h5>
+        </h3>
+        <div className="information">
+        <AboutUs />
+        </div>
       </div>
       <header className="App-header">
       <div className="App-image"></div>
---
+++ src/components/aboutUs.jsx
@@ -0,0 +1,8 @@
+import React from 'react';
+
+export default function AboutUs(props) {
+    
+    return (
+        <div>About Us</div>
+    )
+}
